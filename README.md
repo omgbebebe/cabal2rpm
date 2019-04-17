4c07c35bca1ade4bc3a1e9d4a97d910e00210254
Sergey Bubnov <omg@altlinux.org>
2019-04-17 17:25:01 +0400

    inital commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Usage
+```sh
+cabal2rpm PATH_TO_CABAL_FILE <name | descr | deps | pkgconf ?
+```
+
+# Examples
+```sh
+$ cabal get alsa-core
+Unpacking to alsa-core-0.5.0.1/
+
+$ ./cabal2rpm name alsa-core-0.5.0.1/alsa-core.cabal 
+alsa-core-0.5.0.1
+
+$ ./cabal2rpm descr alsa-core-0.5.0.1/alsa-core.cabal 
+This package provides access to ALSA infrastructure,
+that is needed by both alsa-seq and alsa-pcm.
+
+$ ./cabal2rpm deps alsa-core-0.5.0.1/alsa-core.cabal 
+extensible-exceptions >=0.1.1 && <0.2
+base >=3 && <5
+
+$ ./cabal2rpm pkgconf alsa-core-0.5.0.1/alsa-core.cabal 
+alsa >=1.0.14
+
+$ cabal get xmonad
+Unpacking to xmonad-0.15/
+
+$ ./cabal2rpm name xmonad-0.15/xmonad.cabal 
+xmonad-0.15
+
+$ ./cabal2rpm descr xmonad-0.15/xmonad.cabal 
+xmonad is a tiling window manager for X. Windows are arranged
+automatically to tile the screen without gaps or overlap, maximising
+screen use. All features of the window manager are accessible from the
+keyboard: a mouse is strictly optional. xmonad is written and
+extensible in Haskell. Custom layout algorithms, and other extensions,
+may be written by the user in config files. Layouts are applied
+dynamically, and different layouts may be used on each workspace.
+Xinerama is fully supported, allowing windows to be tiled on several
+screens.
+
+$ ./cabal2rpm deps xmonad-0.15/xmonad.cabal 
+base >=4.9 && <5
+X11 >=1.8 && <1.10
+containers -any
+data-default -any
+directory -any
+extensible-exceptions -any
+filepath -any
+mtl -any
+process -any
+setlocale -any
+unix -any
+utf8-string >=0.3 && <1.1
+
+$ ./cabal2rpm pkgconf xmonad-0.15/xmonad.cabal 
+
+```
---
+++ src/Main.hs
@@ -0,0 +1,67 @@
+module Main where
+
+import Data.Maybe
+import Data.List (intercalate)
+import Distribution.Compiler
+import Distribution.PackageDescription.PrettyPrint
+import Distribution.PackageDescription.Parsec
+import Distribution.Pretty
+import Distribution.Types.BuildInfo
+import Distribution.Types.CondTree
+import Distribution.Types.Dependency
+import Distribution.Types.GenericPackageDescription
+import Distribution.Types.Library
+import Distribution.Types.PackageDescription
+import Distribution.Types.PackageName
+import Distribution.Types.Version
+import Distribution.Types.VersionRange
+import Distribution.System
+import Distribution.Verbosity
+import System.Environment (getArgs)
+
+data PkgInfo = PI { ver :: String
+                  , descr :: String
+                  , deps :: [String]
+                  , pkgconf :: [String]
+                  }
+  deriving (Eq, Show)
+
+defOS = Linux
+defArch = X86_64
+defComp :: (CompilerFlavor, Version)
+defComp = (GHC, mkVersion [8,6,3])
+
+main :: IO ()
+main = do
+  [mode, fname] <- getArgs
+  d <- readGenericPackageDescription normal fname
+  let pinfo = parseDeps d
+  case mode of
+    "name" -> putStrLn $ ver pinfo
+    "descr" -> putStrLn $ descr pinfo
+    "deps"  -> putStrLn $ intercalate "\n" $ deps pinfo
+    "pkgconf" -> putStrLn $ intercalate "\n" $ pkgconf pinfo
+
+defTree :: (Monoid d, Monoid a) => CondTree ConfVar d a -> [Flag] -> (d, a)
+defTree t fs = simplifyCondTree toDef t
+  where toDef cv = case cv of
+                     OS x -> Right $ x == defOS
+                     Arch x -> Right $ x == defArch
+                     Flag x -> Right $ defFlag x
+                     Impl c v -> Right $ (c == (fst defComp)) && withinRange (snd defComp) v
+        defFlag x = fromMaybe False $ lookup x $ map (\f -> (flagName f, flagDefault f)) fs
+
+parseDeps :: GenericPackageDescription -> PkgInfo
+parseDeps d =
+  let pd = packageDescription d
+      ls = case condLibrary d of Nothing -> []; Just x -> [x]
+      flags = genPackageFlags d
+      dt = defTree (fromJust . condLibrary $ d) flags
+      deps' = map prettyShow $ fst dt
+      pkgconf' = map prettyShow $ pkgconfigDepends . libBuildInfo . snd $ dt
+  in
+  PI {ver = prettyShow (package pd)
+     ,descr = description pd
+     ,deps = deps'
+     ,pkgconf = pkgconf'
+     }
